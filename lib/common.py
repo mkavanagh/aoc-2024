843ea5547462dc7c2397a778990314e81268e8fb
Matthew Kavanagh <mattk@brandwatch.com>
2024-12-03 22:04:17 +0000

    Extract common functions

---
+++ lib/common.py
@@ -0,0 +1,28 @@
+"""Common utilities (IO etc)."""
+
+
+def read_columns(filename: str) -> list[list[int]]:
+    """Read a file containing columnar lists of integers (with equal length)."""
+    lists = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            for i, x in enumerate(line.split()):
+                if i == len(lists):
+                    lists.append([])
+                lists[i].append(int(x))
+    return lists
+
+
+def read_rows(filename: str) -> list[list[int]]:
+    """Read a file containing rows of integers, one row per line."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        return [
+            [int(x) for x in line.split()]
+            for line in file
+        ]
+
+
+def read_binary(filename: str) -> bytes:
+    """Read a binary file as bytes."""
+    with open(filename, 'rb') as file:
+        return file.read()
--- main.py
+++ main.py
@@ -3,31 +3,7 @@
 import argparse, sys
 from typing import Optional
 
-
-def read_columns(filename: str) -> list[list[int]]:
-    """Read a file containing columnar lists of integers (with equal length)."""
-    lists = []
-    with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            for i, x in enumerate(line.split()):
-                if i == len(lists):
-                    lists.append([])
-                lists[i].append(int(x))
-    return lists
-
-
-def read_rows(filename: str) -> list[list[int]]:
-    """Read a file containing rows of integers, one row per line."""
-    with open(filename, 'r', encoding='utf-8') as file:
-        return [
-            [int(x) for x in line.split()]
-            for line in file
-        ]
-
-
-def read_binary(filename: str) -> bytes:
-    with open(filename, 'rb') as file:
-        return file.read()
+from lib.common import read_binary, read_columns, read_rows
 
 
 def get_distance(filename: str) -> int:
