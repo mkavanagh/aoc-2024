b7b841ab2f7837a15f91ee201c347f7926f6ffad
Matthew Kavanagh <mattk@brandwatch.com>
2024-12-10 22:53:48 +0000

    Improve disk block representation

--- day09/commands.py
+++ day09/commands.py
@@ -1,16 +1,39 @@
 from lib.common import line_input
 
 
+class Block:
+    __slots__ = ('id', 'length', 'tail')
+
+    id: int
+    length: int
+    tail: int
+
+    def __init__(self, id, length, tail):
+        self.id = id
+        self.length = length
+        self.tail = tail
+
+    def describe(self) -> str:
+        return (str(self.id) * self.length) + ('.' * self.tail)
+
+
 @line_input()
 def get_disk_blocks(lines: list[str]) -> str:
     """Show a representation of the disk blocks for Day 8."""
-    return _parse_blocks(lines[0])
+    return _describe_blocks(_parse_blocks(lines[0]))
 
 
-def _parse_blocks(disk_map: str) -> str:
-    return ''.join(
-        str(int(i / 2)) * int(value)
-        if i % 2 == 0
-        else '.' * int(value)
-        for i, value in enumerate(disk_map)
-    )
+def _parse_blocks(disk_map: str) -> list[Block]:
+    lengths = disk_map[::2]
+    tails = disk_map[1::2]
+    if len(tails) < len(lengths):
+        tails += '0'
+
+    return [
+        Block(str(i), int(lengths[i]), int(tails[i]))
+        for i in range(0, int((len(disk_map) + 1) / 2))
+    ]
+
+
+def _describe_blocks(blocks: list[Block]) -> str:
+    return ''.join(block.describe() for block in blocks)
